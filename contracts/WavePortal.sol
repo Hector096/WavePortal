b3c9b5601016896df2ed39ff303f4860e1ebfd55
Hector <vv96rca@gmail.com>
2022-11-30 23:01:23 +0530

    update waveportal smart contract

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -7,6 +7,7 @@ import "hardhat/console.sol";
 contract WavePortal {
 
     uint256 totalWaves;
+    uint256 private seed;
 
 
 
@@ -21,12 +22,17 @@ contract WavePortal {
     Wave[] waves;
     constructor() payable {
   console.log("We have been constructed!");
+  seed = (block.timestamp + block.difficulty) % 100;
 }
+
+
     function wave (string memory _message) public {
 totalWaves += 1;
  console.log("%s waved w/ message %s", msg.sender, _message);
 waves.push(Wave(msg.sender, _message, block.timestamp));
-emit NewWave(msg.sender, block.timestamp, _message);
+seed = (block.difficulty + block.timestamp + seed) % 100;
+
+ if (seed < 50) {
 uint256 prizeAmount = 0.0001 ether;
 require(
         prizeAmount <= address(this).balance,
@@ -35,6 +41,9 @@ require(
 console.log("%s has waved!",msg.sender);
 (bool success, ) = (msg.sender).call{value: prizeAmount}("");
     require(success, "Failed to withdraw money from contract.");
+ }
+emit NewWave(msg.sender, block.timestamp, _message);
+
     }
 
     function getAllWaves() public view returns (Wave[] memory) {
